250b48d82fc6ffe26bc041a84f11859ead93aaf0
baehyunsol <baehyunsol@naver.com>
2025-08-21 20:37:19 +0900

    add test case

---
+++ test/timestamp-create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE log (
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    content TEXT,
+    updated_at TIMESTAMP WITHOUT TIME ZONE
+);
---
+++ test/timestamp-insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO log (id, title, content, updated_at) VALUES ("abcdef", "test", "hello, world!", DATETIME("now"));
